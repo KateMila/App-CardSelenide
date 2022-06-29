83863278d7ae3ca405a3578c1a07590d19f47770
KateMila <89808903+KateMila@users.noreply.github.com>
2022-06-29 23:10:02 +0300

    Initial commit

---
+++ src/test/java/ru/netology/web/CardDeliveryTest.java
@@ -0,0 +1,19 @@
+package ru.netology.web;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardDeliveryTest {
+
+}
